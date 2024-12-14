f64f2500f5eff4d58ade5b924b4c8e54cb66d76c
Taye Abidakun <tayeabidakun@gmail.com>
2024-12-14 21:30:32 +0100

    testing this

---
+++ index.js
@@ -0,0 +1,21 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+  }
+  
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+app.get("/", (req, res)=>{
+    res.send("This is the landing page");
+});
+app.get("/about", (req, res)=>{
+    res.send("This is the about page");
+})
+
+
+
+
+app.listen(port, ()=>{
+    console.log("Connected");
+})
+
