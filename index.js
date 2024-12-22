514f9a72bc5fdb9fbf7513f7b689b91b5e2c8105
Taye Abidakun <tayeabidakun@gmail.com>
2024-12-22 15:28:07 +0100

    checking process.env file

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+console.log(process.env);
 app.get("/", (req, res)=>{
     res.send("This is the landing page");
 });
